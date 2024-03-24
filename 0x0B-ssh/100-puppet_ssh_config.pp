f3520df7f2242699cb812c83d3c66cac026d8dbc
Dev-Nonso <chinonsoogbonnaya47@gmail.com>
2024-03-24 00:07:29 -0700

    Using puppet to change configuration file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#using puppet to change our configurations file
+
+file {'etc/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+}
